943b35546a995c284da4e19af746b82001672c65
Oguntamu Adeniyi <adeniyioguntamu@gmail.com>
2024-01-18 15:02:54 +0100

    hero image and title edit

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -7,9 +7,7 @@ const Welcome = () => {
       <HeroOverlay
         pageTitle={"SIGNAL VIEWING AND ARCHIVING PORTAL (SVAP)"}
         pageAbreviation={"SVAP"}
-        overlayURL={
-          "https://www.military.africa/wp-content/uploads/2021/07/nigerian-air-force-jf-17-thunder-airstrike-scaled.jpg"
-        }
+        overlayURL={"/public/hero.jpg"}
       />
     </>
   );
