48b49dd7f276ac61f5b69e1c39996722ccea9733
Oguntamu Adeniyi <adeniyioguntamu@gmail.com>
2024-01-19 13:51:59 +0100

    ....

--- frontend/src/pages/uploadpage/UploadPanel.jsx
+++ frontend/src/pages/uploadpage/UploadPanel.jsx
@@ -94,8 +94,8 @@ const UploadPanel = () => {
         {
           headers: {
             Authorization: `Bearer ${profile.token}`,
-            "content-type": "multipart/form-data", // Make sure to set Content-Type
-            "Access-Control-Allow-Headers": "content-type",
+            "Content-Type": "multipart/form-data", // Make sure to set Content-Type
+            "Access-Control-Allow-Headers": "Content-Type",
           },
         }
       );
