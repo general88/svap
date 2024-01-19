d4ffaac0e2787a210a4be39a3c72abe53d454706
Oguntamu Adeniyi <adeniyioguntamu@gmail.com>
2024-01-19 12:49:19 +0100

    removed header edit

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use(function (req, res, next) {
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
   );
   res.setHeader(
-    "Access-Control-Allow-Headers: Content-Type",
+    "Access-Control-Allow-Headers",
     "X-Requested-With,Content-Type, authorization"
   );
   res.setHeader("Access-Control-Allow-Credentials", true);
