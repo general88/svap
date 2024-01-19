b4a815086b9d25bce82ba65438b3c72e4da7da76
Oguntamu Adeniyi <adeniyioguntamu@gmail.com>
2024-01-19 17:51:51 +0100

    fixing cors errors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 // const path = require("path");
 const path = require("node:path");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 // custom middleware
 const { routeNotFoundMiddleware } = require("./middleware/routeNotFound");
@@ -22,68 +22,36 @@ const { connectCloudinary } = require("./config/cloudinaryConnect");
 const authRoutes = require("./route/authRoute");
 const documentRoutes = require("./route/documentRoute");
 // middlewares
+const allowedOrigins = [
+  "https://svap-v2es.vercel.app",
+  "https://localhost:5173",
+];
 
-// const allowedOrigins = [
-//   "https://svap-v2es.vercel.app",
-//   "https://localhost:5173",
-// ];
+// // middleware
+app.use(function (req, res, next) {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, authorization"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
 app.use(express.json());
 // Middleware to parse urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 //
-app.use(function (req, res, next) {
-  // Specify the allowed origins
-  res.setHeader("Access-Control-Allow-Origin", [
-    "https://svap-v2es.vercel.app",
-    "https://localhost:5173",
-  ]);
-
-  // Specify the allowed methods
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-
-  // Specify the allowed headers
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-
-  // Allow credentials
-  res.setHeader("Access-Control-Allow-Credentials", true);
-
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  // Pass control to the next middleware
-  next();
-});
 
 /****** */
-
-// // middleware
-// app.use(function (req, res, next) {
-//   const origin = req.headers.origin;
-
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader("Access-Control-Allow-Origin", origin);
-//   }
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "X-Requested-With,content-type, authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-//
 
 // routes usage
 app.use("/api/v1/auth", authRoutes);
